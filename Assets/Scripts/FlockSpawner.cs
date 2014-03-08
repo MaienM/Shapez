f168a8abbffbff5caaa76871bc63b415bff7babc
Michon van Dooren <michon1992@gmail.com>
2014-03-08 17:28:06 +0100

    Spawn a flock on click.

---
+++ Assets/Scripts/FlockSpawner.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+class FlockSpawner : MonoBehaviour
+{
+    public GameObject prefab;
+
+    public void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Instantiate(prefab, position, Quaternion.identity);
+        }
+    }
+}
+
